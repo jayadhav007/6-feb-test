764f1d3fc01eb0e026780a5aa5d6ad9137844fa2
jayadhav007 <96857887+jayadhav007@users.noreply.github.com>
2022-03-06 19:47:31 +0530

    Add files via upload

---
+++ 6-FEB-TEST/index.js
@@ -0,0 +1,18 @@
+function detail1()
+{
+var product=document.getElementById("img1");
+var pd=document.getElementById("product1");
+pd.style.display=product.click?"block":"none";
+}
+function detail2()
+{
+var product=document.getElementById("img2");
+var pd=document.getElementById("product2");
+pd.style.display=product.click?"block":"none";
+}
+function detail3()
+{
+var product=document.getElementById("img3");
+var pd=document.getElementById("product3");
+pd.style.display=product.click?"block":"none";
+}
